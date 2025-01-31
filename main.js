fa6f876e8ee6f6dbf9239d70b8457c482df0c171
ZJason5958 <zjason5958@gmail.com>
2025-01-30 20:16:37 -0500

    Change

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ var buttons = document.querySelectorAll("button");
 buttons.forEach(element => element.addEventListener("click", toggle));
 
 function toggle() {
-    if (this.className == "") {
+    if (!this.classList.contains("selected")) {
         buttons.forEach(element => element.className = "");
         this.className = "selected";
+        var shown = document.querySelector("ul:not([hide])");
+        var hidden = document.querySelector(".hide");
+        shown.classList.add("hide");
+        hidden.classList.remove("hide")
     }
 }
